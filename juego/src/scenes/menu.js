b22e3093f47bb95461e6b3f680d6cb9a2f5868b9
David-Yanez <luis.yanez@epn.edu.ec>
2022-06-22 23:26:57 -0500

    creacion escena

--- juego/src/scenes/menu.js
+++ juego/src/scenes/menu.js
@@ -1,11 +1,20 @@
 import Phaser from "phaser";
+import { principal } from "./principal.js";
+import { configuracion } from "./configuracion.js";
+
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  backgroundColor: "#000000",
-  parent: "phaser-example",
-  scene: [SceneA, SceneB]
+  backgroundColor: "#640000",
+  // parent: "phaser-example",
+  scene: [principal, configuracion]
 };
 
-let game = new Phaser.Game(config);
+// eslint-disable-next-line
+const game = new Phaser.Game(config);
+
+function al() {
+  alert("Hola menu");
+}
+al();
